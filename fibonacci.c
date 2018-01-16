4759135f8f332979d10810d77911c31c7b061673
Hugo Pérez <hugoa.perezh@gmail.com>
2018-01-16 11:52:31 -0600

    Bug Fixed

--- fibonacci.c
+++ fibonacci.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-
+  int n, primerNum = 0, segundoNum = 1, sigNumero, c;
   printf("Introduce la cantidad a imprimir\n");
-  scanf("%s", &n);
+  scanf("%d", &n);
 
-  printf("Los primeros %s numeros de la serie Fibonacci son: \n", n);
+  printf("Los primeros %d numeros de la serie Fibonacci son: \n", n);
 
   for (c = 0; c < n; c++) {
     if (c <= 1) {
